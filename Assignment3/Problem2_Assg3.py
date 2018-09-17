00f09cff3b1fa270f51532c594284206f01bd6ca
Jane Data <analytics2go@gmail.com>
2018-09-17 10:01:18 -0400

    Add files via upload

--- Assignment3/Problem2_Assg3.py
+++ Assignment3/Problem2_Assg3.py
@@ -1,53 +1,36 @@
 # ----------------------------------------------
-# CPSC-442-11/Python  - Assignment 3 Problem 1
+# CPSC-442-11/Python  - Assignment 3 Problem 2
 # Author:  Wofford, Juana 1014901
-#
 # Program Problem:
 # Given a matrix, which needs to be horizontally flipped and inverted.
 # Flipping the matrix horizontally means each row is reversed.
 # Example, row [ 1 0 0 ] would be flipped to [ 0 0 1 ].
 # Then you need to invert the matrix i.e. replace 0 by 1 and 1 by 0.
-# Example 1:
-# Input: [[1,1,0],[1,0,1],[0,0,0]]
-# Output: [[1,0,0],[0,1,0],[1,1,1]]
-# Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
-#     Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
 # -----------------------------------------
-
-# define function
+# call functions to reverse string and print
 def main():
     starting_string = ['[1,1,0]','[1,0,1]','[0,0,0]']
     print('Input String: ')
     print_list(starting_string)  # print original list
     reverse(starting_string)
-#   y = ['[0,1,1],[1,0,1],[0,0,0]]']
-    invert(starting_string)
-
+#   invert(starting_string)
 
 def reverse(my_string):
     y = ''
-    index = len(my_string)
+    index = len(my_string)  # assigne lenght of string to index
     while index > 0:
         y += my_string[index-1]
-        index = index -1
-    print('Reversed String: ')
+        index = index -1          # reverse the string
+    print('\nReversed String: ')
     print_list(y)
-
 
 def invert(my_string):
     inverted =''.join(reversed(my_string))
-    print('Reveresed - ', inverted)   # 1st reverse
     for x in inverted:
-        if x == '1':
-           print('hi')
+        if x == '1':    # change the 1 to zeros
+            print('....needed more time to understand how todo')
 
-
-
-# funciton to print list
 def print_list(o):
-    for i in o: print(i, end=' ', flush=True)
-    print('\n')
-
-
+    for i in o: print(i, end=' ', flush=True)    # print list
 
 if __name__ == '__main__': main()
--- Assignment3/Problem5_Assgn5.py
+++ Assignment3/Problem5_Assgn5.py
@@ -1,5 +1,5 @@
 # ----------------------------------------------
-# CPSC-442-11/Python  - Assignment 3 Problem 1
+# CPSC-442-11/Python  - Assignment 3 Problem 5
 # Author:  Wofford, Juana 1014901
 # Problem 5
 #  You are given a square 2-D array and each value
@@ -10,16 +10,44 @@
 # new 2-D array that shows the maximum possible heights.
 # Also, you need to return the total sum of height changes.
 # ---------
-
-
 # square 2-D array and each value
 # is termed as the height of each building
-grid1 = [[3,0,8,4]]
-grid2 = [[2,4,5,7]]
-grid3 = [[9,2,6,3]]
-grid4 = [[0,3,1,0]]
+
+def maxheight(grid):
+    nbr_rows = len(grid)   #number of rows
+    nbr_columns = len(grid[0])  #number of columns
+    sum = 0
+    for i in range(nbr_rows):
+        max = 0
+        for j in range(nbr_columns):
+            if grid[i][j] > max:
+               max = grid[i][j]
+ #       print('Max Number in Each Row: ', max)
+        sum += grid[i][j]
+        return sum
+
+g = [[3, 0, 8, 4], [2, 4, 5, 7], [9, 2, 6, 3], [0, 3, 1, 0]]
+# print(grid[0])
+answer  = maxheight(g)
+print(answer)
+
+"""
+sum = 0
+for row in grid:
+    for element in row:
+        print(element, end =' ')
+        print()
+
+for row in grid:
+    print(' '.join([str(element) for element in row]))
+
+# adds all elements
+for row in grid:
+    for element in row:
+        sum += element
+print(sum)
+
+"""
 
 
-total = sum(sum(grid,[]))
-print(total)
 
